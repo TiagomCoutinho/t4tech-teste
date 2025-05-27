67e9d0153d99cc21d76b2d84db7a96c07c392df7
Tiago Coutinho <25138805+TiagomCoutinho@users.noreply.github.com>
2025-05-27 20:48:31 -0300

    Feat: Initial Players Datatable

---
+++ frontend/src/components/PlayersDatatable.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import { usePlayersStore } from '@/stores/players'
+
+const playersStore = usePlayersStore()
+</script>
+
+<template>
+  <DataTable :value="playersStore.getAllPlayers()" paginator :rows="25" class="w-full table-auto">
+    <Column field="id" sortable header="ID" class="alou-id" />
+    <Column field="first_name" sortable header="First Name" />
+    <Column field="last_name" header="Last Name" />
+  </DataTable>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import PlayersDatatable from '@/components/PlayersDatatable.vue'
+</script>
+
 <template>
   <div class="home">
     <h1>This is the home page</h1>
+    <PlayersDatatable />
   </div>
 </template>
