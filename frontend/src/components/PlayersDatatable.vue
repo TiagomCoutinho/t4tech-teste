a840f805e57a90bb9d68f827ccf7a1d4b1cf0149
Tiago Coutinho <25138805+TiagomCoutinho@users.noreply.github.com>
2025-05-27 21:56:23 -0300

    Feat: start of the search button

--- frontend/src/components/PlayersDatatable.vue
+++ frontend/src/components/PlayersDatatable.vue
@@ -2,8 +2,16 @@
 import { ref } from 'vue'
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
+import InputText from 'primevue/inputtext'
 import { usePlayersStore } from '@/stores/players'
 import IconEdit from '@/components/icons/IconEdit.vue'
+import { FilterMatchMode } from '@primevue/core/api'
+
+const globalFilter = ref('')
+
+const filter = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+})
 
 const columns = ref([
   { field: 'id', header: 'ID', sortable: true },
@@ -20,37 +28,45 @@ const passthroughClasses = {
 </script>
 
 <template>
-  <DataTable
-    :value="playersStore.getAllPlayers()"
-    paginator
-    :rows="25"
-    class="w-full table-auto"
-    :pt="passthroughClasses"
-  >
-    <Column
-      v-for="column in columns"
-      :key="column.field"
-      :field="column.field"
-      :header="column.header"
-      :sortable="column.sortable"
-      v-slot:body="slotProps"
+  <div>
+    <InputText
+      v-model="filter.global.value"
+      placeholder="Search by name"
+    />
+    <DataTable
+      :value="playersStore.getAllPlayers()"
+      paginator
+      :rows="25"
+      class="w-full table-auto"
+      :pt="passthroughClasses"
+      :globalFilterFields="['first_name', 'last_name']"
+      v-model:filters="filter"
     >
-      <template v-if="column.field === 'first_name'">
-        {{ slotProps.data.first_name }} {{ slotProps.data.last_name }}
-      </template>
-      <template v-else>
-        {{ slotProps.data[column.field] }}
-      </template>
-    </Column>
-    <Column>
-      <template #header>
-        <span>Edit</span>
-      </template>
-      <template #body="{ data }">
-        <button class="opacity-50 hover:opacity-100 cursor-pointer" @click="console.log('Edit', data.id)">
-          <IconEdit />
-        </button>
-      </template>
-    </Column>
-  </DataTable>
+      <Column
+        v-for="column in columns"
+        :key="column.field"
+        :field="column.field"
+        :header="column.header"
+        :sortable="column.sortable"
+        v-slot:body="slotProps"
+      >
+        <template v-if="column.field === 'first_name'">
+          {{ slotProps.data.first_name }} {{ slotProps.data.last_name }}
+        </template>
+        <template v-else>
+          {{ slotProps.data[column.field] }}
+        </template>
+      </Column>
+      <Column>
+        <template #header>
+          <span>Edit</span>
+        </template>
+        <template #body="{ data }">
+          <button class="opacity-50 hover:opacity-100 cursor-pointer" @click="console.log('Edit', data.id)">
+            <IconEdit />
+          </button>
+        </template>
+      </Column>
+    </DataTable>
+  </div>
 </template>
